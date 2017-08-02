c375c3e32baabd75044b3708583a18a70421f632
sysyushuo <sysmenghuan@163.com>
2017-08-02 21:54:28 +0800

    Merge branch 'master' into day-05

--- www/orm.py
+++ www/orm.py
@@ -13,7 +13,7 @@ def log(sql, args=()):
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
-    __pool = yield from aiomysql.create_pool(
+    __pool =  aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
         user=kw['user'],
